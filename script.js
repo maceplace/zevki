6ba4e4a33f1d0764d3a57496d22e8637e9324d38
Mace Dev <bulava.place@gmail.com>
2024-12-12 18:03:45 +0700

    asasa

--- script.js
+++ script.js
@@ -6,15 +6,12 @@ switch (hash) {
   // Website
   case '#md':
     window.location.replace("https://macestudios.ru");
-    break;
   // YouTube
   case '#yt':
     window.location.replace("https://youtube.com/@macedev?si=tfRWVtvo7UA0w1Of");
-    break;
   // VK
   case '#vk':
     window.location.replace("https://vk.com/macedev");
-    break;
   // No redirect
   default:
     console.log("Can't redirect the " + hash)
